7361fd928ed15f8f5caf32a09d3e828a39b4dbff
Jansen <jansen@opticpower.com>
2021-12-19 02:00:34 -0400

    Update memeScore.js

--- pages/api/memeScore.js
+++ pages/api/memeScore.js
@@ -4,18 +4,9 @@ const token = process.env.SLACK_TOKEN;
 const channel = process.env.SLACK_CHANNEL;
 const slack = new WebClient(token, { logLevel: 'debug' });
 
-const handler = nextConnect({
-  onError(error, _, res) {
-    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
-  },
-  onNoMatch(req, res) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-  },
-});
-
-handler.get(async (req, res) => {
+export default async function memeScore(_, res) {
   try {
-    const response = await slack.reactions.get({ channel });
+    const response = await slack.conversations.history({ channel });
     console.log('response', response);
   } catch (e) {
     console.log('e', e);
@@ -25,12 +16,4 @@ handler.get(async (req, res) => {
   //['keycap_ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two', 'one'])
 
   res.status(200).json({ data: 'success', response });
-});
-
-export default handler;
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+}
